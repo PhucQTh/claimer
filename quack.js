690251953eba8c3c2a244eb072c4fe3eea54089e
IT-THUONG QUANG PHUC <phuc.thuong@api.local>
2024-06-11 08:21:05 +0700

    add new function: hatch egg

--- quack.js
+++ quack.js
@@ -1,9 +1,13 @@
 const TELEGRAM_USER = require('./token.json');
+const MAX_DUCK = 17;
+const MAX_DUCK_RARITY = 5;
 let ACCESS_TOKEN = TELEGRAM_USER.state.token;
 
 let listColect = [];
 let listDuck = [];
 let listReward = [];
+let listHatch = [];
+let listRemoveDuck = [];
 let time_to_golden_duck = 0;
 Array.prototype.random = function () {
     return this[Math.floor(Math.random() * this.length)];
@@ -71,15 +75,30 @@ async function getListCollectEgg() {
         let data = await response.json();
         // console.log(data);
         if (data.error_code !== '') console.log(data.error_code);
-        data.data.duck && (listDuck = data.data.duck);
-        // console.log(data.data.nest);
-        listColect = data.data.nest.filter((item) => item.type_egg);
+        data.data.duck &&
+            (listDuck = data.data.duck.filter(
+                (item) => item.total_rare > MAX_DUCK_RARITY - 1
+            ));
 
+        // listDuck.filter((item) => item.total_rare > 4);
+        listRemoveDuck = data.data.duck.filter(
+            (item) => item.total_rare === MAX_DUCK_RARITY - 1
+        );
+        listColect = data.data.nest.filter((item) => item.type_egg < 4);
+        listHatch = data.data.nest.filter((item) => item.type_egg > 3);
         let eggs = listColect.map((i) => i.id);
-
+        if (listRemoveDuck.length > 0) {
+            console.log(`Duck can remove: ${listRemoveDuck.length}`);
+            await removeDuck();
+        }
         if (listColect.length > 0) {
             console.log(`So 🥚 co the thu thap: ${listColect.length}`, eggs);
             collect();
+        }
+
+        if (listHatch.length > 0) {
+            console.log(`Eggs can hatch: ${listHatch.length}`);
+            hatchEgg();
         }
     } catch (error) {
         // console.log("getListCollectEgg error:", error);
@@ -88,39 +107,100 @@ async function getListCollectEgg() {
     }
 }
 
-async function collect() {
+const hatchEgg = async () => {
     try {
-        if (listColect.length === 0) return getTotalEgg();
-
-        const egg = listColect[0];
-        // console.log(egg);
-
-        let response = await fetch(
-            'https://api.quackquack.games/nest/collect',
-            {
-                headers: {
-                    accept: '*/*',
-                    'accept-language': 'en-US,en;q=0.9,vi;q=0.8',
-                    authorization: 'Bearer ' + ACCESS_TOKEN,
-                    'content-type': 'application/x-www-form-urlencoded',
-                },
-                body: 'nest_id=' + egg.id,
-                method: 'POST',
-            }
-        );
-        let data = await response.json();
-        // console.log(data);
-
-        if (data.error_code !== '') console.log(data.error_code);
-
-        const duck = getDuckToLay();
-        layEgg(egg, duck);
+        const egg = listHatch[0];
+        let response = await fetch('https://api.quackquack.games/nest/hatch', {
+            headers: {
+                accept: '*/*',
+                'accept-language': 'en-US,en;q=0.9,vi;q=0.8',
+                authorization: 'Bearer ' + ACCESS_TOKEN,
+                'content-type': 'application/x-www-form-urlencoded',
+            },
+            body: 'nest_id=' + egg.id,
+            method: 'POST',
+        });
+        let { data } = await response.json();
+        console.log(data);
+        const time_claim = data['time_claim'];
+        const time_to_claim = time_claim - Date.now();
+        setTimeout(() => {
+            collectDuck(egg.id);
+        }, time_to_claim);
     } catch (error) {
-        // console.log("collect error:", error);
-        console.log('Mat ket noi collect, thu lai sau 3s');
-        setTimeout(collect, 3e3);
+        console.log('Mat ket noi hatch, thu lai sau 3s');
+        // setTimeout(hatch, 3e3);
     }
-}
+};
+const collectDuck = (nestId) => {
+    try {
+        fetch('https://api.quackquack.games/nest/collect-duck', {
+            headers: {
+                accept: '*/*',
+                'accept-language': 'en-US,en;q=0.9,vi;q=0.8',
+                authorization: 'Bearer ' + ACCESS_TOKEN,
+                'content-type': 'application/x-www-form-urlencoded',
+            },
+            body: 'nest_id=' + nestId,
+            method: 'POST',
+        });
+    } catch (error) {
+        // console.log("hatch error:", error);
+        console.log('Mat ket noi hatch, thu lai sau 3s');
+        // setTimeout(hatch, 3e3);
+    }
+};
+const removeDuck = () => {
+    try {
+        listRemoveDuck = listRemoveDuck.map(async (item) => {
+            const res = await fetch(
+                'https://api.quackquack.games/duck/remove',
+                {
+                    headers: {
+                        accept: '*/*',
+                        'accept-language': 'en-US,en;q=0.9,vi;q=0.8',
+                        authorization: 'Bearer ' + ACCESS_TOKEN,
+                        'content-type': 'application/x-www-form-urlencoded',
+                    },
+                    body: `ducks=%7B+%22ducks%22%3A+%5B%22${item.id}%22%5D+%7D`,
+                    method: 'POST',
+                }
+            );
+        });
+    } catch (error) {}
+};
+const collect = async () => {
+    if (listColect.length === 0) {
+        return getTotalEgg();
+    }
+
+    const egg = listColect.shift();
+    let data;
+    try {
+        data = await fetch('https://api.quackquack.games/nest/collect', {
+            headers: {
+                accept: '*/*',
+                'accept-language': 'en-US,en;q=0.9,vi;q=0.8',
+                authorization: 'Bearer ' + ACCESS_TOKEN,
+                'content-type': 'application/x-www-form-urlencoded',
+            },
+            body: 'nest_id=' + egg.id,
+            method: 'POST',
+        }).then((res) => res.json());
+    } catch (error) {
+        console.log('Mat ket noi collect, thu lai sau 3s');
+        await new Promise((resolve) => setTimeout(resolve, 3e3));
+        return collect();
+    }
+
+    if (data.error_code !== '') {
+        console.log(data.error_code);
+        return collect();
+    }
+
+    const duck = getDuckToLay();
+    layEgg(egg, duck);
+};
 
 function getDuckToLay() {
     let duck = null;
@@ -152,8 +232,6 @@ async function layEgg(egg, duck) {
             }
         );
         let data = await response.json();
-        // console.log(data);
-
         if (data.error_code !== '') {
             console.log(data.error_code);
             const duck = getDuckToLay();
